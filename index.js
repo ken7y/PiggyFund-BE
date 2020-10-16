1c29625754297f8e769b8a828effad22bd23b722
Ken <kenwang2227@gmail.com>
2020-10-16 13:53:48 +1100

    viewall and add operation added

--- index.js
+++ index.js
@@ -2,9 +2,52 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://test:test@cluster0.cbjkw.mongodb.net/PiggyFund?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/getall', (req, res) => {
+  client.connect(err => {
+  let arr = [];
+  var cursor = client.db("PiggyFund").collection('Spendings').find();
+  cursor.each(function(err, item) {
+      if(item == null) {
+          client.close(); 
+          res.send(arr);
+          return;
+      }
+      arr.push(item);
+  });
+  });
+})
+
+
+app.get('/spending', (req, res) => {
+  // http://localhost:3000/spending?category=Clothes&amount=50
+  var category = req.query.category ? req.query.category : "Food";
+  var amount = req.query.amount ? req.query.amount : "0";
+  var currency = req.query.currency ? req.query.currency : "Aud";
+  var now = new Date();
+  var isoString = now.toISOString();
+
+  client.connect(err => {
+  let arr = [];
+  let requestJson = {"category": category, "amount" : amount, "currency" : currency, "Time": isoString}
+  var cursor = client.db("PiggyFund").collection('Spendings');
+
+  cursor.insertOne(requestJson);
+
+  });
+
+  res.send("done");
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
